844cb2e12b6a6f97200f428e5c529397dd58a606
Billy.Zheng <vil963@gmail.com>
2026-01-01 22:56:42 +0800

    Refactor: use --pattern option more CLI way

--- src/cli.cr
+++ src/cli.cr
@@ -13,21 +13,30 @@ USAGE
 
 filenames = ARGV
 default_pattern = ["src/**/*.cr", "spec/**/*.cr", "lib/**/*.cr"]
+reset_default_pattern = false
 project = false
 tag_name = "./CTAGS"
+new_tag_name = false
 
 OptionParser.parse do |parser|
   parser.banner = usage
 
-  parser.on("-p PATTERN", "--pattern=PATTERN", "Specify the file pattern used to create the CTAGS index.
-e.g. --pattern='[\"src/**/*.cr\", \"spec/**/*.cr\"]'
+  parser.on("-p PATTERN", "--pattern=PATTERN", "Specify the glob pattern (can be repeated) used to create the CTAGS index.
+e.g. --pattern=\"src/**/*.cr\" --pattern \"spec/**/*.cr\"
 Specifying this option implicitly includes --project.") do |pattern|
-    default_pattern = pattern
+    if reset_default_pattern == false
+      reset_default_pattern = true
+      default_pattern = [] of String
+    end
+
+    default_pattern << pattern
+
     project = true
   end
 
-  parser.on("-o tagfile", "--output=TAGFILE", "TAG file name, , default: #{tag_name}") do |name|
+  parser.on("-o tagfile", "--output=TAGFILE", "TAG file name, default: #{tag_name}") do |name|
     tag_name = name
+    new_tag_name = true
   end
 
   parser.on("--project", "Generate CTAGS for current project, default: #{default_pattern}") do
@@ -60,10 +69,17 @@ end
 if project
   STDERR.puts "Writing #{tag_name} for current project use pattern: #{default_pattern}"
 
-  pattern = default_pattern.is_a?(String) ? Array(String).from_json(default_pattern.as(String)) : default_pattern
-  filenames = Dir.glob(pattern)
+  filenames = Dir.glob(default_pattern)
 
-  File.write(tag_name, CrystalCtags::Ctags.new(filenames)) unless filenames.empty?
+  abort "Can't found file result use #{default_pattern}" if filenames.empty?
+
+  File.write(tag_name, CrystalCtags::Ctags.new(filenames))
 else
-  puts CrystalCtags::Ctags.new(ARGV)
+  abort "Can't found file result use #{ARGV}" if ARGV.empty?
+
+  if new_tag_name
+    File.write(tag_name, CrystalCtags::Ctags.new(ARGV))
+  else
+    puts CrystalCtags::Ctags.new(ARGV)
+  end
 end
